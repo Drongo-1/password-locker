668b4b14c447637ffe969dc6d1fdc096154ca8b8
Drongo-1 <ngugidavid46@gmail.com>
2021-02-14 19:09:46 +0000

    modified class user

--- user_credentials.py
+++ user_credentials.py
@@ -7,8 +7,28 @@ global users_list
 
 
 class User:
-        Class method that copies a credential's info after the credential's site name is entered
+    '''
+    Class to create user accounts and save their information
+    '''
+    # Class Variables
+    # global users_list
+    users_list = []
+
+    def __init__(self, first_name, last_name, password):
         '''
-        find_credential = Credential.find_by_site_name(site_name)
-        return pyperclip.copy(find_credential.password)
+        Method to define the properties for each user object will hold.
+        '''
+
+        # instance variables
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+
+    def save_user(self):
+        '''
+        Function to save a newly created user instance
+        '''
+        User.users_list.append(self)
+
+
 
